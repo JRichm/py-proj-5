ee753586d3e025ce850b1d808b5eff8a5423fc08
JRichm <jblackenmusic@gmail.com>
2023-05-08 13:51:15 -0600

    created db tables

--- model.py
+++ model.py
@@ -1,15 +1,49 @@
 """Models for movie ratings app."""
 
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name = db.Column(db.String(255), unique=True, nullable=False)
+    user_password = db.Column(db.String(255), nullable=False)
+    user_email = db.Column(db.String(255), nullable=True)
+    
+    def __repr__(self):
+        return f'<User Data:\n\t{self.user_name} ({self.user_id})\n\t{self.user_email}>'
+        
+class Rating(db.Model):
+    __tablename__ = 'ratings'
+    
+    rating_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    rating_score = db.Column(db.Integer, nullable=False)
+    rating_movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'), nullable=False)
+    rating_user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    
+    movie = db.relationship('Movie', backref='ratings')
+    user = db.relationship('User', backref='ratings')
+    
+    def __repr__(self):
+        return f'<Rating Data\n\tRating ID: {self.rating_id}\n\tMovie ID: {self.rating_movie_id}\n\tScore: {self.rating_score}\n\tUser: {self.rating_user_id}>'
 
-# Replace this with your code!
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    
+    movie_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    movie_title = db.Column(db.String(255), nullable=False)
+    movie_description = db.Column(db.Text(), nullable=True)
+    movie_release_date = db.Column(db.DateTime(), nullable=False)
+    
+    def __repr__(self):
+        return f'<Movie Data\n\t{self.movie_title} ({self.movie_id})\n\t{self.movie_description}\n\t{self.movie_release_date}>'
 
 
 def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
-    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["POSTGRES_URI"]
     flask_app.config["SQLALCHEMY_ECHO"] = echo
     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
